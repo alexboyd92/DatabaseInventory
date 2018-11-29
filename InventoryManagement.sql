062aec56c60783fbb15cf0cbdf7ae23767660f52
Alex Boyd <33331083+alexboyd92@users.noreply.github.com>
2018-11-29 14:48:49 -0500

    Add files via upload

---
+++ InventoryManagement.sql
@@ -0,0 +1,81 @@
+-- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+--
+-- Host: localhost    Database: invMang
+-- ------------------------------------------------------
+-- Server version	5.7.24-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `invDetails`
+--
+
+DROP TABLE IF EXISTS `invDetails`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invDetails` (
+  `invID` int(11) NOT NULL,
+  `ItemName` varchar(40) NOT NULL,
+  `ManfName` varchar(40) NOT NULL,
+  `qunaity` int(11) DEFAULT NULL,
+  `par` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ItemName`,`ManfName`),
+  KEY `invID` (`invID`),
+  CONSTRAINT `invDetails_ibfk_1` FOREIGN KEY (`invID`) REFERENCES `invSheet` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invDetails`
+--
+
+LOCK TABLES `invDetails` WRITE;
+/*!40000 ALTER TABLE `invDetails` DISABLE KEYS */;
+/*!40000 ALTER TABLE `invDetails` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invSheet`
+--
+
+DROP TABLE IF EXISTS `invSheet`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invSheet` (
+  `ID` int(11) NOT NULL,
+  `user_name` varchar(40) DEFAULT NULL,
+  `buissnes_name` varchar(40) DEFAULT NULL,
+  `dateCreated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invSheet`
+--
+
+LOCK TABLES `invSheet` WRITE;
+/*!40000 ALTER TABLE `invSheet` DISABLE KEYS */;
+INSERT INTO `invSheet` VALUES (1,'alex','Name','2018-11-20 21:41:23');
+/*!40000 ALTER TABLE `invSheet` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-11-20 16:44:24
